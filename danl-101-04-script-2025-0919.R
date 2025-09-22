4f743dcef1db4e491e41abbf519c6d9b4ff2990e
bcdanl <bchoe@geneseo.edu>
2025-09-22 00:01:59 -0400

    class

---
+++ danl-101-04-script-2025-0919.R
@@ -0,0 +1,21 @@
+a <- 1
+
+library(tidyverse) # load tidyverse package
+mpg # this is mpg data.frame
+
+
+# read_csv() read CSV file 
+df <- read_csv("https://bcdanl.github.io/data/spotify_all.csv")
+# 
+
+
+# section 1 ---------------------------------------------------------------
+
+
+# section 2 ---------------------------------------------------------------
+
+
+
+
+
+
